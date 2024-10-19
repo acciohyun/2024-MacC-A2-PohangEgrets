3fef062f2dd3ad9037f42c953f10b9316754aff9
leewanjae <dhkswo1@gmail.com>
2024-10-19 12:58:56 +0900

    project init

---
+++ pohang_egrets_watch Watch App/pohang_egrets_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  pohang_egrets_watchApp.swift
+//  pohang_egrets_watch Watch App
+//
+//  Created by LeeWanJae on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct pohang_egrets_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
