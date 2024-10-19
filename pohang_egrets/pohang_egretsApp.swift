4af9531425004d98474c3522dad5827f965e8e37
leewanjae <dhkswo1@gmail.com>
2024-10-19 12:50:00 +0900

    project init

---
+++ pohang_egrets/pohang_egretsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pohang_egretsApp.swift
+//  pohang_egrets
+//
+//  Created by LeeWanJae on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct pohang_egretsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
