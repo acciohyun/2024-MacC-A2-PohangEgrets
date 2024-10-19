46f5846ded03f62a0995b1377d2c8fb6fc18eb40
Hyun Lee (Mia) <leeseunghyun7799@gmail.com>
2024-10-20 01:14:28 +0900

    [#1] - LiveWorkoutUseCase 구현 + 그에 필요한 기타 파일 구현 + HealthKit이랑 Background Mode를 Info.plist에 추가

---
+++ pohang_egrets/Source/Domain/Entity/WorkoutSummary.swift
@@ -0,0 +1,14 @@
+//
+//  WorkoutSummary.swift
+//  pohang_egrets
+//
+//  Created by Hyun Lee on 10/20/24.
+//
+
+import Foundation
+
+struct WorkoutSummary {
+    var averageHeartRate: String
+    var timeExercised: String
+    var totalCalories: String
+}
--- pohang_egrets/Source/Domain/UseCase/Shared/WorkoutSummaryUseCase.swift
+++ pohang_egrets/Source/Domain/UseCase/Shared/WorkoutSummaryUseCase.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-class WorkoutSummary: ObservableObject {
+class WorkoutSummaryUseCase: ObservableObject {
     
 }
